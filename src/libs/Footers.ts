0014ab79a63a70022e3a9d6bc219e4564888664f
Will-Warin <warintorn_sr@cmu.ac.th>
2026-09-03 14:24:08 +0700

    n1

---
+++ src/libs/Footers.ts
@@ -0,0 +1,8 @@
+// 📋 src/libs/Footer.ts
+interface FooterProps {
+  year: string;
+  fullName: string;
+  studentId: string | number; // union type: string หรือ number ก็ได้
+}
+
+export type { FooterProps };
